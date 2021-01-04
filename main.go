cfa1301d32f46dbc364febf2d93bc608ce56590b
Haniye Mollaei <HaniyeMolaei1378@gmail.com>
2021-01-04 20:53:18 +0330

    edit main

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/ie/supermarket-server/handler"
-	"github.com/ie/supermarket-server/model"
 	"github.com/labstack/echo/v4"
 )
 
@@ -18,20 +16,9 @@ func main() {
 
 	e.GET("/report/:month", handler.GetReport)
 	e.GET("/report", handler.GetGeneralReport)
+
 	if err := e.Start("0.0.0.0:8080"); err != nil {
 		fmt.Println("error in start server : ", err)
 	}
 
-	c := model.Customer{
-		Name:    "haniye",
-		ID:      1,
-		Address: "tehran",
-		Tel:     1234,
-		//cRegisterDate:
-	}
-	b, err := json.Marshal(c)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(b))
 }
